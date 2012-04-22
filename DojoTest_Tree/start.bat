8941333609162cfa58cdebf3a5e508cfeb67e00e
Khalik Jandos <jandos.kh@gmail.com>
2012-04-22 22:48:33 +0900

    test

---
+++ DojoTest_Tree/start.bat
@@ -0,0 +1,4 @@
+"C:\Documents and Settings\Jandos\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" 
+C:\projects\LearningDojo\DojoTest_Tree\DojoTest.html 
+--allow-file-access-from-files 
+
