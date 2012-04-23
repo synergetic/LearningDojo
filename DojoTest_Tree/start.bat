a31c70f139813b32ff95e896ac30ab030ba775e3
jandos <jandos.kh@gmail.com>
2012-04-23 19:57:24 +0900

    a

--- DojoTest_Tree/start.bat
+++
@@ -1,4 +0,0 @@
-"C:\Documents and Settings\Jandos\Local Settings\Application Data\Google\Chrome\Application\chrome.exe" 
-C:\projects\LearningDojo\DojoTest_Tree\DojoTest.html 
---allow-file-access-from-files 
-
